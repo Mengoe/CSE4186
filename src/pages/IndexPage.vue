bf4a67168ed6d828b111778dc8fae84241dcb2da
Mengou <gjsehdrh@gmail.com>
2024-03-30 00:23:46 +0900

    💄 make RouterLink to route signUp page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,12 +4,19 @@
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
+    />
+    <RouterLink
+      id="signUp"
+      to="/signUp"
+      class="q-ml-xl text-h2 text-weight-bold"
     >
+      회원가입 페이지로!
+    </RouterLink>
   </q-page>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
