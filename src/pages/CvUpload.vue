9ec146702a29b3e397636f4b38ac1e53e0d192da
Mengou <gjsehdrh@gmail.com>
2024-04-04 23:10:02 +0900

    💄 separate cv registration and question generation

--- src/pages/CvUpload.vue
+++ src/pages/CvUpload.vue
@@ -1,48 +1,129 @@
 <template>
-  <q-page class="row items-stretch justify-center">
-    <div style="width: 100%" class="column items-center">
-      <q-carousel
-        v-model="store.slide"
-        transition-prev="jump-right"
-        transition-next="jump-left"
-        swipeable
-        animated
-        padding
-        height="100%"
-        style="width: 100%"
-        class="text-dark shadow-1 rounded-borders"
-      >
-        <q-carousel-slide name="style" class="column no-wrap flex-center">
-          <FirstCvSlide />
-        </q-carousel-slide>
-        <q-carousel-slide name="tv" class="column no-wrap flex-center">
-          <SecondCvSlide />
-        </q-carousel-slide>
-      </q-carousel>
-      <ProgressBar :progress="progress" />
+  <q-page class="q-gutter-y-lg column flex-center">
+    <div class="cv-title text-h4 text-blue-8 text-weight-bold">
+      자기소개서를 입력해주세요!
     </div>
+    <q-form
+      @submit="onSubmit"
+      class="q-gutter-y-md column flex-center items-stretch"
+      style="width: 50%"
+    >
+      <q-input
+        v-model="title"
+        outlined
+        label="자기소개서의 제목을 입력해주세요."
+        lazy-rules
+        :rules="cvRules"
+      >
+        <template v-slot:prepend>
+          <q-icon :name="outlinedTitle"></q-icon>
+        </template>
+      </q-input>
+      <q-input
+        v-model="content"
+        outlined
+        type="textarea"
+        placeholder="내용을 입력해주세요."
+        input-style="min-height:400px; max-height:400px"
+        lazy-rules
+        :rules="cvRules"
+      />
+      <q-btn
+        v-if="!isDoneRegister"
+        :loading="loading"
+        type="submit"
+        color="primary"
+        text-color="white"
+        size="xl"
+      >
+        <div>자기소개서 등록하기!</div>
+        <q-icon :name="outlinedAddCircle" right></q-icon>
+      </q-btn>
+      <q-btn
+        v-else
+        color="primary"
+        text-color="white"
+        size="xl"
+        @click="toCvLists"
+      >
+        면접 보러가기
+      </q-btn>
+    </q-form>
   </q-page>
 </template>
 
 <script setup>
-import { computed, ref } from "vue";
-import FirstCvSlide from "components/FirstCvSlide.vue";
-import SecondCvSlide from "components/SecondCvSlide.vue";
-import ProgressBar from "components/ProgressBar.vue";
-import { matArrowBack, matArrowForward } from "@quasar/extras/material-icons";
-import { useCvStore } from "src/stores/cv";
-import { onMounted } from "vue";
-const store = useCvStore();
+import { ref } from "vue";
+import { useQuasar } from "quasar";
+import {
+  outlinedAddCircle,
+  outlinedTitle,
+} from "@quasar/extras/material-icons-outlined";
+import { useRouter } from "vue-router";
+import axios from "axios";
 
-const progress = computed(() => store.progress);
+const $q = useQuasar();
+const router = useRouter();
+const cvRules = [(val) => (val && val !== "") || "내용을 입력해주세요!"];
 
-onMounted(() => {
-  store.$reset();
-});
+const title = ref("");
+const content = ref("");
+const loading = ref(false); // 등록 대기 flag
+const isDoneRegister = ref(false); // 자기소개서 등록 완료 됐는지를 나타내는 flag
+
+const tmpUserId = 1;
+//const genAPI = `https://259da068-0fdc-4898-8a3d-28d48fa2de21.mock.pstmn.io/post/cv/${tmpUserId}`;
+const cvPostAPI =
+  "https://259da068-0fdc-4898-8a3d-28d48fa2de21.mock.pstmn.io/post/cv";
+
+function onSubmit() {
+  loading.value = true;
+
+  try {
+    let cvData = {};
+    cvData.title = title.value;
+    cvData.content = content.value;
+    // 유저 id는 api 경로에 추가해서 보냄
+    axios
+      .post(cvPostAPI, JSON.stringify(cvData), {
+        headers: {
+          "Content-Type": `application/json`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        // fail 처리 해야 됨
+        if (response.data.message === "fail") {
+        } else {
+          // 성공
+          $q.notify({
+            position: "center",
+            icon: "done",
+            color: "primary",
+            message: `${response.data.message}`,
+            timeout: 800,
+          });
+
+          isDoneRegister.value = true;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        loading.value = false;
+      });
+  } catch (error) {
+    console.log(error);
+    loading.value = false;
+  }
+}
+
+function toCvLists() {
+  router.push("/cvList");
+}
 </script>
 
-<style lang="scss" scoped>
-.q-carousel__slide {
-  justify-content: flex-start;
-}
-</style>
+<style lang="scss" scoped></style>
+
+cvUpload
