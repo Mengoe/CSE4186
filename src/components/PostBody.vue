16b1e3aa9ab15eb567c8079037237a60a03bedf1
Mengou <gjsehdrh@gmail.com>
2024-04-08 18:14:02 +0900

    ✨ add post detail page & related component

---
+++ src/components/PostBody.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="body text-h5 text-dark q-mt-lg q-ml-lg">
+    {{ props.content }}
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  content: String,
+});
+</script>
